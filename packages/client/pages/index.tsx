a24775da6d557ebdcb7466e710a9d07db7c728ad
tayutaedomo <tayutaedomo@gmail.com>
2023-08-26 00:16:31 +0900

    Section 2 Lesson 2 is completed

---
+++ packages/client/pages/index.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next';
+import Link from 'next/link';
+
+import Layout from '@/components/layout/Layout';
+import styles from '@/styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Welcome to Messenger 📫</h1>
+          <div className={styles.card}>
+            <Link href="/message/SendMessagePage">
+              <h2>send &rarr;</h2>
+            </Link>
+            <p>send message and avax to other accounts</p>
+          </div>
+          <div className={styles.card}>
+            <Link href="/message/ConfirmMessagePage">
+              <h2>check &rarr;</h2>
+            </Link>
+            <p>Check messages from other accounts</p>
+          </div>
+        </main>
+      </div>
+    </Layout>
+  );
+}
