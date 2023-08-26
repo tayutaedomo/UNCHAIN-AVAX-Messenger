749e52469e686e65c3e6b6e409538b968f31dac6
tayutaedomo <tayutaedomo@gmail.com>
2023-08-26 19:15:25 +0900

    Section 2 Lesson 3 is completed

--- packages/client/pages/message/ConfirmMessagePage.tsx
+++ packages/client/pages/message/ConfirmMessagePage.tsx
@@ -1,10 +1,14 @@
 import { BigNumber } from 'ethers';
 
-import MessageCard from '../../components/card/MessageCard';
-import Layout from '../../components/layout/Layout';
-import { Message } from '../../hooks/useMessengerContract';
+import MessageCard from '@/components/card/MessageCard';
+import Layout from '@/components/layout/Layout';
+import RequireWallet from '@/components/layout/RequireWallet';
+import { Message } from '@/hooks/useMessengerContract';
+import { useWallet } from '@/hooks/useWallet';
 
 export default function ConfirmMessagePage() {
+  const { currentAccount, connectWallet } = useWallet();
+
   const message: Message = {
     depositInWei: BigNumber.from('1000000000000000000'),
     timestamp: new Date(1),
@@ -17,17 +21,22 @@ export default function ConfirmMessagePage() {
 
   return (
     <Layout>
-      {ownMessages.map((message, index) => {
-        return (
-          <div key={index}>
-            <MessageCard
-              message={message}
-              onClickAccept={() => {}}
-              onClickDeny={() => {}}
-            />
-          </div>
-        );
-      })}
+      <RequireWallet
+        currentAccount={currentAccount}
+        connectWallet={connectWallet}
+      >
+        {ownMessages.map((message, index) => {
+          return (
+            <div key={index}>
+              <MessageCard
+                message={message}
+                onClickAccept={() => {}}
+                onClickDeny={() => {}}
+              />
+            </div>
+          );
+        })}
+      </RequireWallet>
     </Layout>
   );
 }
--- packages/client/pages/message/SendMessagePage.tsx
+++ packages/client/pages/message/SendMessagePage.tsx
@@ -1,16 +1,25 @@
-import SendMessageForm from '../../components/form/SendMessageForm';
-import Layout from '../../components/layout/Layout';
+import SendMessageForm from '@/components/form/SendMessageForm';
+import Layout from '@/components/layout/Layout';
+import RequireWallet from '@/components/layout/RequireWallet';
+import { useWallet } from '@/hooks/useWallet';
 
 export default function SendMessagePage() {
+  const { currentAccount, connectWallet } = useWallet();
+
   return (
     <Layout>
-      <SendMessageForm
-        sendMessage={(
-          text: string,
-          receiver: string,
-          tokenInEther: string,
-        ) => {}}
-      />
+      <RequireWallet
+        currentAccount={currentAccount}
+        connectWallet={connectWallet}
+      >
+        <SendMessageForm
+          sendMessage={(
+            text: string,
+            receiver: string,
+            tokenInEther: string,
+          ) => {}}
+        />
+      </RequireWallet>
     </Layout>
   );
 }
